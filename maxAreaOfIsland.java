8110f9b191aa666f1ff5f9eaf245c50f261d1d50
pawar-srinidhi <srinidhipawar96@gmail.com>
2025-09-15 13:24:03 +0530

    Day-26 solutions

---
+++ maxAreaOfIsland.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int rows=grid.length;
+        int cols=grid[0].length;
+        int maxArea=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1){
+                    maxArea=Math.max(maxArea,getCurrentArea(i,j,grid));
+                }
+            }
+        }
+        return maxArea;
+    }
+    private int getCurrentArea(int i,int j,int [][]grid){
+        if(i<0 || j<0|| i>=grid.length ||j>=grid[0].length||grid[i][j]<=0){
+            return 0;
+        }
+        grid[i][j]=-1;
+        int leftArea=getCurrentArea(i,j-1,grid);
+        int rightArea=getCurrentArea(i,j+1,grid);
+        int upArea=getCurrentArea(i-1,j,grid);
+        int downArea=getCurrentArea(i+1,j,grid);
+        int totalArea=leftArea+rightArea+upArea+downArea;
+        return 1+totalArea;
+    }
+}
